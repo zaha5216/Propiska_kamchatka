5057e56409cbf68d9a7a979b471d66b848b4fb38
zaha5216 <51168482+zaha5216@users.noreply.github.com>
2019-10-31 22:56:01 +0300

    Update settings.py

--- Propiska41/settings.py
+++ Propiska41/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import psycopg2
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,6 +91,9 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # DATABASES = {
 #     'default': {
