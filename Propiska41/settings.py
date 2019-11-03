8bfbb454b55554fe27cc345913216942eb970c3c
Zaha5216 <zaha5216@.gmail.com>
2019-11-03 04:56:03 +0300

    my message

--- Propiska41/settings.py
+++ Propiska41/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import psycopg2
-import django_heroku
+# import django_heroku
 import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
