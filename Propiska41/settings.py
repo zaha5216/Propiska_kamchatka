ff888eb94730a1eb1a03658f3773a14dbd0f2af4
Zaha5216 <zaha5216@.gmail.com>
2019-11-04 04:23:02 +0300

    my message

--- Propiska41/settings.py
+++ Propiska41/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = '2d0a9oxl6$$s09-4a^r@5&c(xmx*%&fmfq3$2248lu^r-z@eer'
 DEBUG = True
 
 
-# ALLOWED_HOSTS = ['damp-savannah-98220.herokuapp.com']
+# ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['damp-savannah-98220.herokuapp.com']
 
 
 # Application definition
